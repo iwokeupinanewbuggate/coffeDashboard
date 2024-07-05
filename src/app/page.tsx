b78dfa3f6fa2af0f27adc5124f158aafca4403ba
24HP9677 24HP9677 <24HP9677@G303-CC0136.local>
2024-07-05 17:37:59 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#543A20",justifyContent:"center",alignItems:"center",display:"flex" }}
+    >
+      <div style={{backgroundColor:"#CE9760",width:"80vw",height:"90vh",borderRadius:"40px"}}>
+        
+        
+       </div>
+    </div>
+  );
+}
